fa389fba355a40a1e49689e19e363132536ea95e
dab2000 <dmbasov@gmail.com>
2025-06-22 01:28:02 +0300

    add marker context menu with MarkerContextmenu component

--- src/combo-tool.js
+++ src/combo-tool.js
@@ -10,6 +10,7 @@ import {
   LocaleService,
   LayoutService,
   CapitalVizabiService,
+  MarkerContextmenu,
   TreeMenu,
   SteppedSlider,
   Dialogs,
@@ -72,6 +73,11 @@ export default class Combo extends BaseComponent {
       model: marker,
       name: "tree-menu"
     },{
+      type: MarkerContextmenu,
+      placeholder: ".vzb-marker-contextmenu",
+      model: marker,
+      name: "marker-contextmenu"
+    },{
       type: DataWarning,
       placeholder: ".vzb-datawarning",
       options: {appendButtonHere: ".vzb-tool-combo"},
@@ -122,6 +128,7 @@ export default class Combo extends BaseComponent {
       <div class="vzb-buttonlist"></div>
     </div>
     <div class="vzb-treemenu"></div>
+    <div class="vzb-marker-contextmenu"></div>
     <div class="vzb-datawarning"></div>
     <div class="vzb-spaceconfig"></div>
     <div class="vzb-datanotes"></div>
