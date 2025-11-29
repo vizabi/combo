ffc2b48451129382a70f13a900409d7220a720ca
Angie Hjort <konzeptmeister@gmail.com>
2025-11-29 21:59:13 +0100

    v1.4.5: Autocfg update for legends

--- src/combo-tool.js
+++ src/combo-tool.js
@@ -503,13 +503,23 @@ Combo.DEFAULT_MODEL = {
         }
         //"scale": { "ref": "markers.bubble.encoding.color.scale" }
       },
-      "name": { "data": {  } },
+      "name": {
+        "data": {
+          "concept": {"filter": { "concept": { "$in": ["name"]} } }
+        }
+      },
       "order": {
         "modelType": "order",
         "direction": "asc",
-        "data": { }
+        "data": {
+          "concept": {"filter": { "concept": { "$in": ["rank"]} } }
+        }
       },
-      "map": { "data": { } }
+      "map": { 
+        "data": {
+          "concept": {"filter": { "concept": { "$in": ["shape_lores_svg", "shape", "svg"]} } }
+        }
+      }
     }
   },
   "legend_map": {
@@ -531,13 +541,23 @@ Combo.DEFAULT_MODEL = {
         }
         //"scale": { "ref": "markers.bubble.encoding.color.scale" }
       },
-      "name": { "data": { } },
+      "name": { 
+        "data": {
+          "concept": {"filter": { "concept": { "$in": ["name"]} } }
+        }
+      },
       "order": {
         "modelType": "order",
         "direction": "asc",
-        "data": { }
+        "data": {
+          "concept": {"filter": { "concept": { "$in": ["rank"]} } }
+        }
       },
-      "map": { "data": { } }
+      "map": { 
+        "data": {
+          "concept": {"filter": { "concept": { "$in": ["shape_lores_svg", "shape", "svg"]} } }
+        }
+      }
     }
   }
 };
